f971d352b539fca921575033f2e10295b13be412
Jaemin Choi <jaemin.choi@cp.kaist.ac.kr>
2022-10-05 14:08:01 +0900

    separate helpers

--- theories/code.v
+++ theories/code.v
@@ -5,7 +5,7 @@ From iris.program_logic Require Import atomic.
 From iris.proofmode Require Import proofmode.
 From iris.heap_lang Require Import proofmode notation.
 From iris.prelude Require Import options.
-(*From chase_lev Require Import helpers.*)
+From chase_lev Require Import helpers.
 
 (*
   We use a finite length list without resizing;
@@ -74,65 +74,6 @@ Definition dequeΣ : gFunctors :=
 Global Instance subG_dequeΣ {Σ} : subG dequeΣ Σ → dequeG Σ.
 Proof. solve_inG. Qed.
 
-
-
-Section helper.
-  Context `{!heapGS Σ} (N : namespace).
-
-  Lemma array_agree x l1 l2 dq1 dq2 :
-    length l1 = length l2 →
-    x ↦∗{dq1} l1 -∗ x ↦∗{dq2} l2 -∗ ⌜l1 = l2⌝.
-  Proof.
-    revert l2.
-    iInduction l1 as [|v l1] "HInd" using rev_ind;
-      iIntros (l2 HL) "x1 x2".
-      { destruct l2; auto. }
-    destruct l2 using rev_ind.
-      { rewrite app_length in HL. simpl in HL. lia. }
-    clear IHl2.
-
-    do 2 rewrite array_app.
-    assert (length l1 = length l2).
-      { do 2 rewrite app_length in HL. simpl in HL. lia. }
-    iDestruct "x1" as "[x1 xl1]". iDestruct "x2" as "[x2 xl2]".
-    do 2 rewrite array_singleton. rewrite H.
-    iDestruct (mapsto_agree with "xl1 xl2") as "<-".
-    iDestruct ("HInd" $! l2 with "[] [x1] [x2]") as "<-"; auto.
-  Qed.
-End helper.
-
-Section slice.
-  Context {A : Type}.
-  Implicit Types l : list A.
-
-  (* k elements starting from i-th *)
-  Definition slice l i k := take k (drop i l).
-
-  Lemma slice_0 l i : slice l i 0 = [].
-  Proof. auto. Qed.
-
-  Lemma slice_insert_right l i k j v :
-    j >= i + k →
-    slice (<[j:=v]> l) i k = slice l i k.
-  Proof.
-    unfold slice. intros H.
-    rewrite drop_insert_le; [|lia].
-    rewrite take_insert; auto. lia.
-  Qed.
-
-  Lemma slice_extend_right l i k v :
-    l !! (i+k) = Some v →
-    slice l i (S k) = slice l i k ++ [v].
-  Proof.
-    unfold slice. intros H.
-    rewrite (take_S_r _ _ v); auto.
-    by rewrite lookup_drop.
-  Qed.
-End slice.
-
-
-
-
 Section proof.
   Context `{!heapGS Σ, !dequeG Σ} (N : namespace).
   Notation iProp := (iProp Σ).
@@ -154,7 +95,6 @@ Section proof.
   Global Instance is_deque_persistent γq q :
     Persistent (is_deque γq q) := _.
 
-  (* TODO: use ghost_var? *)
   Definition deque_content (γq : gname) (frag : list val) : iProp :=
     ghost_var γq (1/2) frag.
 
--- theories/helpers.v
+++ theories/helpers.v
@@ -1,4 +1,31 @@
 From stdpp Require Import list.
+From iris.proofmode Require Import proofmode.
+From iris.heap_lang Require Import proofmode notation.
+
+Section heap.
+  Context `{!heapGS Σ} (N : namespace).
+
+  Lemma array_agree x l1 l2 dq1 dq2 :
+    length l1 = length l2 →
+    x ↦∗{dq1} l1 -∗ x ↦∗{dq2} l2 -∗ ⌜l1 = l2⌝.
+  Proof.
+    revert l2.
+    iInduction l1 as [|v l1] "HInd" using rev_ind;
+      iIntros (l2 HL) "x1 x2".
+      { destruct l2; auto. }
+    destruct l2 using rev_ind.
+      { rewrite app_length in HL. simpl in HL. lia. }
+    clear IHl2.
+
+    do 2 rewrite array_app.
+    assert (length l1 = length l2).
+      { do 2 rewrite app_length in HL. simpl in HL. lia. }
+    iDestruct "x1" as "[x1 xl1]". iDestruct "x2" as "[x2 xl2]".
+    do 2 rewrite array_singleton. rewrite H.
+    iDestruct (mapsto_agree with "xl1 xl2") as "<-".
+    iDestruct ("HInd" $! l2 with "[] [x1] [x2]") as "<-"; auto.
+  Qed.
+End heap.
 
 Section slice.
   Context {A : Type}.
@@ -9,4 +36,22 @@ Section slice.
 
   Lemma slice_0 l i : slice l i 0 = [].
   Proof. auto. Qed.
+
+  Lemma slice_insert_right l i k j v :
+    j >= i + k →
+    slice (<[j:=v]> l) i k = slice l i k.
+  Proof.
+    unfold slice. intros H.
+    rewrite drop_insert_le; [|lia].
+    rewrite take_insert; auto. lia.
+  Qed.
+
+  Lemma slice_extend_right l i k v :
+    l !! (i+k) = Some v →
+    slice l i (S k) = slice l i k ++ [v].
+  Proof.
+    unfold slice. intros H.
+    rewrite (take_S_r _ _ v); auto.
+    by rewrite lookup_drop.
+  Qed.
 End slice.
