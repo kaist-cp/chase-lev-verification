047a1d39a330576d71d3ae137563d4847a57aaeb
jh05013 <jaemin.choi@cp.kaist.ac.kr>
2022-11-25 13:50:59 +0900

    cleanup 2

--- theories/circular/helpers.v
+++ theories/circular/helpers.v
@@ -1,5 +1,4 @@
 From stdpp Require Import list.
-From iris.proofmode Require Import proofmode.
 From iris.heap_lang Require Import proofmode notation.
 
 Ltac encode_agree Hγ :=
@@ -30,29 +29,19 @@ Section heap.
   Proof.
     revert l2.
     iInduction l1 as [|v l1] "HInd" using rev_ind;
-      iIntros (l2 HL) "x1 x2".
-      { destruct l2; auto. }
+      iIntros (l2 HL) "x1 x2". { destruct l2; auto. }
     destruct l2 using rev_ind.
       { rewrite app_length in HL. simpl in HL. lia. }
-    clear IHl2.
 
     do 2 rewrite array_app.
-    assert (length l1 = length l2).
+    assert (length l1 = length l2) as Hlen.
       { do 2 rewrite app_length in HL. simpl in HL. lia. }
     iDestruct "x1" as "[x1 xl1]". iDestruct "x2" as "[x2 xl2]".
-    do 2 rewrite array_singleton. rewrite H.
+    do 2 rewrite array_singleton. rewrite Hlen.
     iDestruct (mapsto_agree with "xl1 xl2") as "<-".
-    iDestruct ("HInd" $! l2 with "[] [x1] [x2]") as "<-"; auto.
+    by iDestruct ("HInd" $! l2 with "[] [x1] [x2]") as "<-".
   Qed.
 End heap.
-
-Section nat.
-  Lemma neq_symm (x y : nat) : x ≠ y → y ≠ x.
-  Proof. lia. Qed.
-
-  Lemma lt_mult (n a b : nat) : n ≠ 0 → n*a < n*b → a < b.
-  Proof. intros. induction n; lia. Qed.
-End nat.
 
 Section modulo.
   Lemma rem_mod_eq (x y : nat) : (0 < y) → (x `rem` y)%Z = x `mod` y.
@@ -60,24 +49,33 @@ Section modulo.
     intros Hpos. rewrite Z.rem_mod_nonneg; [rewrite Nat2Z.inj_mod| |]; lia.
   Qed.
 
-  Lemma mod_neq a b n :
+  Lemma lt_mult (n a b : nat) : n*a < n*b → a < b.
+  Proof. induction n; lia. Qed.
+
+  Lemma close_mod_neq a b n :
     a < b < a+n → a `mod` n ≠ b `mod` n.
   Proof.
-    intros H amn. assert (n ≠ 0). 1: lia.
+    intros H Hmod. assert (n ≠ 0) by lia.
     assert (Ha := PeanoNat.Nat.div_mod a n).
     assert (Hb := PeanoNat.Nat.div_mod b n).
     rewrite Ha in H; auto. rewrite Hb in H; auto.
-    rewrite amn in H.
-    assert (n*a`div`n < n*b`div`n < n*a`div`n + n) by lia.
-    replace (n*a`div`n + n) with (n*(a`div`n + 1)) in H1 by lia.
+    rewrite Hmod in H.
+    assert (n*a`div`n < n*b`div`n < n*a`div`n + n) as H' by lia.
+    replace (n*a`div`n + n) with (n*(a`div`n + 1)) in H' by lia.
     assert (a`div`n < b`div`n < a`div`n + 1); try lia.
-    destruct H1 as [H1A H1B].
-    apply lt_mult in H1A; auto. apply lt_mult in H1B; auto.
+    destruct H' as [H1 H2].
+    apply lt_mult in H1; auto. apply lt_mult in H2; auto.
   Qed.
+End modulo.
 
+Section list.
   Context {A : Type}.
   Implicit Types l : list A.
+
+  (* mod set/get *)
+
   Definition mod_set l i v := <[i `mod` length l:=v]> l.
+
   Definition mod_get l i := l !! (i `mod` length l).
 
   Lemma mod_get_is_Some l i :
@@ -104,11 +102,8 @@ Section modulo.
     intros Hij. unfold mod_get, mod_set.
     by rewrite insert_length list_lookup_insert_ne.
   Qed.
-End modulo.
-
-Section list.
-  Context {A : Type}.
-  Implicit Types l : list A.
+  
+  (* slice *)
 
   (* circ_slice l i j = l[i%len..(j-1)%len] *)
   Fixpoint circ_slice_d l i d :=
@@ -136,9 +131,9 @@ Section list.
     remember (j - i) as d. revert i Hij Heqd.
     induction d; intros.
     - simpl. replace i with j by lia. by rewrite Hj.
-    - assert (is_Some (mod_get l i)) as [vi Vi]. 1: by apply mod_get_is_Some.
-      simpl. rewrite Vi. simpl.
-      rewrite <- (IHd (S i)); try lia. auto.
+    - assert (is_Some (mod_get l i)) as [vi Vi] by by apply mod_get_is_Some.
+      simpl. rewrite Vi; simpl.
+      rewrite -(IHd (S i)); try lia. auto.
   Qed.
 
   Lemma circ_slice_update_right l i j v :
@@ -150,12 +145,13 @@ Section list.
     remember (j - i) as d. revert i Hij Heqd.
     induction d; intros; auto.
     assert (i < j) by lia.
-    assert (is_Some (mod_get l i)) as [vi Vi]. 1: by apply mod_get_is_Some.
+    assert (is_Some (mod_get l i)) as [vi Vi] by by apply mod_get_is_Some.
     assert (mod_get (mod_set l j v) i = Some vi) as Vi'.
-    { rewrite mod_set_get_ne; auto. assert (i < j < i + length l) by lia.
-      apply (mod_neq _ _ (length l)) in H0. lia. }
+    { rewrite mod_set_get_ne; auto.
+      assert (i < j < i + length l) as H' by lia.
+      apply (close_mod_neq _ _ (length l)) in H'. lia. }
     simpl. rewrite Vi Vi'.
-    rewrite <- (IHd (S i)); try lia. auto.
+    rewrite -(IHd (S i)); by try lia.
   Qed.
 
   Lemma circ_slice_shrink_right l i j v :
